c00c58441db57e6fe6b9444cd244dce869fd9343
Sam Button <sam_button_94@hotmail.co.uk>
2022-04-12 23:40:24 +0100

    Finished step 7

--- directory.rb
+++ directory.rb
@@ -13,6 +13,21 @@ students = [
 {name: "Joffrey Baratheon", cohort: :november},
 {name: "Norman Bates", cohort: :november}
 ]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+  name = gets.chomp
+  # while the name is not empty, repeat this
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #students.count} students"
+
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -27,8 +42,9 @@ end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students."
-end  
+end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
